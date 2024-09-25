88b4b57f8f6f80f81a7b6949ecb9f52e6ab9c98a
Petri Korpikoski <petrik.dev@outlook.com>
2024-09-26 02:25:06 +0300

    auctionHouse component
    Started working on the visuals of the auctionHouse component. Added a box-sizing: border-box to the universal * selector in my index.css file. Also made some adjustments to compensate for the changes.

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./HomePageStyles.module.css";
+import portrait from "../../assets/swordsman.png"; /*Temporary */
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <h1>Browse Auctions</h1>
-      <p></p>
+      <div className={styles.auctionHouse}>
+        <div className={styles.classPortrait}>
+          <img
+            className={styles.classImg}
+            src={portrait}
+            alt="Class portrait"
+          />
+        </div>
+        <div className={styles.searchSection}></div>
+      </div>
     </div>
   );
 };
