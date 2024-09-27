70c27529d9dec46faa230df99d71ed0c139ef065
Petri Korpikoski <petrik.dev@outlook.com>
2024-09-27 04:12:13 +0300

    Search & Reset buttons added
    Created a form inside the search container and placed an <input> and two <button> elements inside it. Gave them a flex display and placed them with margins. Chose type="button" for the search button to avoid default behavior (reloading page).

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./HomePageStyles.module.css";
 import portrait from "../../assets/swordsman.png"; /*Temporary */
+import searchIcon from "../../assets/Magnifying_glass_png.png";
+import searchGif from "../../assets/Magnifying_glass_gif.gif";
 
 const Home = () => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <div className={styles.container}>
       <h1>Browse Auctions</h1>
@@ -14,7 +19,25 @@ const Home = () => {
             alt="Class portrait"
           />
         </div>
-        <div className={styles.searchSection}></div>
+        <div className={styles.searchSection}>
+          <form>
+            <input
+              type="text"
+              placeholder="Name"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
+            <img
+              src={isFocused ? searchGif : searchIcon}
+              alt="Search"
+              className={styles.inputIcon}
+            />
+            <div className={styles.buttonContainer}>
+              <button type="button">Search</button>
+              <button type="reset">Reset</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
