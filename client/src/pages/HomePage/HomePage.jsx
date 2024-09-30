3fa799065b6e9909072ce1e29064b7c485cfa7ba
Petri Korpikoski <petrik.dev@outlook.com>
2024-09-30 04:17:05 +0300

    Added level range inputs
    Styled and added two new text style inputs for the appropriate level ranges. It only accepts numbers and two digits. I used the type="text", because it removes the issue of symbols that type="number" allows.
    
    Also, added styling to the index.css to remove potential spinner buttons from type="number" inputs. Might be unnecessary in the future.

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "./HomePageStyles.module.css";
-import portrait from "../../assets/swordsman.png"; /*Temporary */
+import portrait from "../../assets/swordsman.png"; // Temporary
 import searchIcon from "../../assets/Magnifying_glass_png.png";
 import searchGif from "../../assets/Magnifying_glass_gif.gif";
 
 const Home = () => {
   const [isFocused, setIsFocused] = useState(false);
+  const [levelMin, setLevelMin] = useState("");
+  const [levelMax, setLevelMax] = useState("");
+
+  const handleLevelChange = (e, setValue) => {
+    const value = e.target.value;
+
+    if (/^\d{0,2}$/.test(value)) {
+      setValue(value);
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -32,9 +41,42 @@ const Home = () => {
               alt="Search"
               className={styles.inputIcon}
             />
+            <div className={styles.rangeContainer}>
+              <input
+                className={styles.levelRange}
+                type="text"
+                value={levelMin}
+                min="1"
+                max="60"
+                placeholder="Min"
+                onChange={(e) => handleLevelChange(e, setLevelMin)}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+              />
+              <p id={styles.levelRange}>-</p>
+              <input
+                className={styles.levelRange}
+                type="text"
+                value={levelMax}
+                min="1"
+                max="60"
+                placeholder="Max"
+                onChange={(e) => handleLevelChange(e, setLevelMax)}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+              />
+            </div>
             <div className={styles.buttonContainer}>
               <button type="button">Search</button>
-              <button type="reset">Reset</button>
+              <button
+                type="reset"
+                onClick={() => {
+                  setLevelMin("");
+                  setLevelMax("");
+                }}
+              >
+                Reset
+              </button>
             </div>
           </form>
         </div>
