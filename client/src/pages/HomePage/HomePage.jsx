d001b868f3aa9c279530f52c3d5e37845994c837
Petri Korpikoski <petrik.dev@outlook.com>
2024-10-02 02:57:52 +0300

    CategorySection + icons
    Created the category section and gave appropriate icons to represent each category.

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,16 @@ import searchIcon from "../../assets/Magnifying_glass_png.png";
 import searchGif from "../../assets/Magnifying_glass_gif.gif";
 import previousImg from "../../assets/previous_img.png";
 import nextImg from "../../assets/next_img.png";
+import weaponsCategory from "../../assets/weapons_category.png";
+import armorCategory from "../../assets/armor_category.png";
+import potionsCategory from "../../assets/potions_category.png";
+import enchantmentsCategory from "../../assets/enchantments_category.png";
+import craftingCategory from "../../assets/crafting_category.png";
+import recipesCategory from "../../assets/recipes_category.png";
+import questItemsCategory from "../../assets/questItems_category.png";
+import bagsCategory from "../../assets/bags_category.png";
+import craftingMaterialsCategory from "../../assets/craftingMaterials_category.png";
+import foodCategory from "../../assets/food_category.png";
 
 const Home = () => {
   const [isFocused, setIsFocused] = useState(false);
@@ -85,13 +95,107 @@ const Home = () => {
                 </button>
               </div>
               <div className={styles.bottomBtnContainer}>
-                <button className={styles.bottomBtn} type="button"></button>
+                <button className={styles.bottomBtn} type="button">
+                  <img
+                    src={previousImg}
+                    alt="Previous"
+                    className={styles.btnIconLeft}
+                  />
+                </button>
                 <p className={styles.prevText}>Prev</p>
                 <p className={styles.nextText}>Next</p>
-                <button className={styles.bottomBtn} type="button"></button>
+                <button className={styles.bottomBtn} type="button">
+                  <img
+                    src={nextImg}
+                    alt="Next"
+                    className={styles.btnIconRight}
+                  />
+                </button>
               </div>
             </div>
           </form>
+        </div>
+        <div className={styles.categorySection}>
+          <button className={styles.category}>
+            Weapons
+            <img
+              src={weaponsCategory}
+              alt="Weapons category"
+              className={styles.categoryIcon}
+            />
+          </button>
+          <button className={styles.category}>
+            Armor
+            <img
+              src={armorCategory}
+              alt="Armor category"
+              className={styles.categoryIcon}
+            />
+          </button>
+          <button className={styles.category}>
+            Potions
+            <img
+              src={potionsCategory}
+              alt="Potions category"
+              className={styles.categoryIcon}
+            />
+          </button>
+          <button className={styles.category}>
+            Food
+            <img
+              src={foodCategory}
+              alt="Food category"
+              className={styles.categoryIcon}
+            />
+          </button>
+          <button className={styles.category}>
+            Enchantments
+            <img
+              src={enchantmentsCategory}
+              alt="Enchantments category"
+              className={styles.categoryIcon}
+            />
+          </button>
+          <button className={styles.category}>
+            Crafting tools
+            <img
+              src={craftingCategory}
+              alt="Crafting tools category"
+              className={styles.categoryIcon}
+            />
+          </button>
+          <button className={styles.category}>
+            Crafting materials
+            <img
+              src={craftingMaterialsCategory}
+              alt="Crafting materials category"
+              className={styles.categoryIcon}
+            />
+          </button>
+          <button className={styles.category}>
+            Recipes
+            <img
+              src={recipesCategory}
+              alt="Recipes category"
+              className={styles.categoryIcon}
+            />
+          </button>
+          <button className={styles.category}>
+            Bags
+            <img
+              src={bagsCategory}
+              alt="Bags category"
+              className={styles.categoryIcon}
+            />
+          </button>
+          <button className={styles.category}>
+            Quest items
+            <img
+              src={questItemsCategory}
+              alt="Quest items category"
+              className={styles.categoryIcon}
+            />
+          </button>
         </div>
       </div>
     </div>
